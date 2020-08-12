64efac4985239bb3ecf972d033e9abdd847288f3
Scott Johnson <scottjohnson@Scotts-MacBook-Air.local>
2020-08-12 12:24:41 -0400

    Scott's Files

---
+++ Scott/1 Review.is.mjs
@@ -0,0 +1,29 @@
+document.getElementById("myP").addEventListener("click", function() {
+    document.getElementById("myP").style.fontSize = "30px";
+  });
+  
+  document.getElementById("myP").innerHTML = "welcome to day 3!";
+  
+  document.getElementById("a").style.width = "200px";
+  document.getElementById("a").style.height = "200px";
+  document.getElementById("a").style.backgroundColor = "#D54E8D";
+  
+  // document.getElementById("table").addEventListener("mouseover", function () {
+  //   alert("Yo, it's an alert");
+  // });
+  
+  document.getElementById("h3Tag").addEventListener("click", function() {
+    document.getElementById("h3Tag").style.color = "gold";
+  });
+  
+  document.getElementById("greninja").addEventListener("mouseover", function() {
+    document.getElementById("greninja").src =
+      "https://www.smashbros.com/assets_v2/img/fighter/greninja/main8.png";
+  });
+  
+  document.getElementById("greninja").addEventListener("mouseout", function() {
+    document.getElementById("greninja").src =
+      "https://www.smashbros.com/assets_v2/img/fighter/greninja/ss_6.jpg";
+  });
+  
+  
